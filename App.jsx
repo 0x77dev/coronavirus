aa5a7dbfd82fc36d952492b9a801b5b999c253d5
Misha Marinenko <0x77dev@protonmail.com>
2020-04-15 21:14:29 +0300

    API UPDATE

--- App.jsx
+++ App.jsx
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     this.state = {
       totalConfirmed: "Loading...",
       totalDeaths: "Loading...",
-      totalRecovered: "Loading..."
+      totalRecovered: "Loading...",
     };
   }
 
@@ -31,30 +31,20 @@ export default class App extends React.Component {
           Developed by Misha Marinenko @0x77dev
         </Text>
         <Text style={{ fontSize: 10, fontWeight: "bold" }}>
-          Data from arcgis.com
+          Data from https://corona.lmao.ninja/all
         </Text>
-        <Text style={{ fontSize: 10 }}>marinenko.rf.gd</Text>
+        <Text style={{ fontSize: 10 }}>blog.0x77.page</Text>
         <Text style={{ fontSize: 10 }}>github.com/0x77dev</Text>
       </View>
     );
   }
   async loadData(vm) {
-    const totalConfirmedFetch = await fetch(
-      "https://services9.arcgis.com/N9p5hsImWXAccRNI/arcgis/rest/services/Z7biAeD8PAkqgmWhxG2A/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Confirmed%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true"
-    );
-    const totalConfirmedFetchJSON = await totalConfirmedFetch.json();
-    const totalDeathsFetch = await fetch(
-      "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Deaths%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true"
-    );
-    const totalDeathsFetchJSON = await totalDeathsFetch.json();
-    const totalRecoveredFetch = await fetch(
-      "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Recovered%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true"
-    );
-    const totalRecoveredFetchJSON = await totalRecoveredFetch.json();
+    const dataFetch = await fetch("https://corona.lmao.ninja/all");
+    const { cases, deaths, recovered } = await dataFetch.json();
     vm.setState({
-      totalConfirmed: totalConfirmedFetchJSON.features[0].attributes.value,
-      totalDeaths: totalDeathsFetchJSON.features[0].attributes.value,
-      totalRecovered: totalRecoveredFetchJSON.features[0].attributes.value
+      totalConfirmed: cases,
+      totalDeaths: deaths,
+      totalRecovered: recovered,
     });
   }
 
@@ -68,6 +58,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+  },
 });
